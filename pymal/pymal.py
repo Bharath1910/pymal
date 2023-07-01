d13319f64a9ea84eb25e93593894037474ed1f5e
Bharath1910 <av.bharath@proton.me>
2023-07-01 10:46:34 +0530

    refactored a bit

--- pymal/pymal.py
+++ pymal/pymal.py
@@ -1,29 +1,39 @@
 import requests
-from .enums import AnimeFields
+
+MAL_API_ENDPOINT = "https://api.myanimelist.net/v2"
 
 class Pymal:
-    clientID = None
+    clientID: str = None
 
-    def __init__(self, clientID):
+    def __init__(self, clientID: str) -> None:
         self.clientID = clientID
 
-        r = requests.get("https://api.myanimelist.net/v2", headers={
+        r = requests.get(MAL_API_ENDPOINT, headers={
             "X-MAL-CLIENT-ID": self.clientID
         })
 
         if r.json()['message'] == 'Invalid client id':
             raise Exception("Invalid client ID")
     
-    def getAnimeList(self, query, limit=4):
-        r = requests.get("https://api.myanimelist.net/v2/anime?q=" + query + "&limit=" + str(limit), headers={
+    def getAnimeList(self, query: str, limit: int = 4) -> list:
+        r = requests.get(f"{MAL_API_ENDPOINT}/anime", headers={
             "X-MAL-CLIENT-ID": self.clientID
+        }, params={
+            "q": query,
+            "limit": limit
         })
 
         return r.json()['data']
     
-    def getAnimeDetails(self, animeId, fields=AnimeFields.ALL):
-        r = requests.get("https://api.myanimelist.net/v2/anime/" + str(animeId) + "?fields=" + fields.value, headers={
+    def getAnimeDetails(self, animeId: int, fields: list = None) -> dict:
+        if fields is not None:
+            fields = [field.value for field in fields]
+            fields = ",".join(fields)
+        
+        r = requests.get(f"{MAL_API_ENDPOINT}/anime/{animeId}", headers={
             "X-MAL-CLIENT-ID": self.clientID
+        }, params={
+            "fields": fields
         })
 
         return r.json()
