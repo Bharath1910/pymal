8bc11b3a51bf26cb62b18fd940ad1dff39431326
Bharath1910 <av.bharath@proton.me>
2023-07-01 12:02:32 +0530

    refactored requests

--- pymal/pymal.py
+++ pymal/pymal.py
@@ -6,45 +6,46 @@ MAL_API_ENDPOINT = "https://api.myanimelist.net/v2"
 class Pymal:
     clientID: str = None
 
+    def __getData(self, pathParams: list[str] = [], queryParams: dict = None) -> dict:
+        if queryParams is None:
+            queryParams = {}
+        
+        r = requests.get(f"{MAL_API_ENDPOINT}/{'/'.join(pathParams)}", headers={
+            "X-MAL-CLIENT-ID": self.clientID
+        }, params=queryParams)
+
+        return r.json()
+
     def __init__(self, clientID: str) -> None:
         self.clientID = clientID
+        res = self.__getData()
 
-        r = requests.get(MAL_API_ENDPOINT, headers={
-            "X-MAL-CLIENT-ID": self.clientID
-        })
-
-        if r.json()['message'] == 'Invalid client id':
+        if res['message'] == 'Invalid client id':
             raise Exception("Invalid client ID")
     
     def getAnimeList(self, query: str, limit: int = 4) -> list:
-        r = requests.get(f"{MAL_API_ENDPOINT}/anime", headers={
-            "X-MAL-CLIENT-ID": self.clientID
-        }, params={
+        res = self.__getData(['anime'], {
             "q": query,
             "limit": limit
         })
 
-        return r.json()['data']
+        return res['data']
     
     def getAnimeDetails(self, animeId: int, fields: list = None) -> dict:
         if fields is not None:
             fields = [field.value for field in fields]
             fields = ",".join(fields)
-        
-        r = requests.get(f"{MAL_API_ENDPOINT}/anime/{animeId}", headers={
-            "X-MAL-CLIENT-ID": self.clientID
-        }, params={
+
+        res = self.__getData(['anime', str(animeId)], {
             "fields": fields
         })
 
-        return r.json()
+        return res
     
     def getAnimeRanking(self, rankingType: Ranking, limit: int = 100):
-        r = requests.get(f"{MAL_API_ENDPOINT}/anime/ranking", headers={
-            "X-MAL-CLIENT-ID": self.clientID
-        }, params={
+        res = self.__getData(['anime', 'ranking'], {
             "ranking_type": rankingType.value,
             "limit": limit
         })
 
-        return r.json()['data']
+        return res['data']
